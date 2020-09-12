0ada9f4fa1c1062cf89ea78e5cb75f0e1981fcef
janholo <janholo@users.noreply.github.com>
2020-09-12 21:59:04 +0000

    Deploying to gh-pages from  @ e790f5d760a86548f23d44e4805980182e828e09 🚀

---
+++ precache-manifest.7dd3b081b776bb1ab5e5c6bbcbe0e645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537c4b9c262392cd5503a2523c36a533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3c64d7a89a09431afa9",
+    "url": "/static/css/main.c9d2f3cb.chunk.css"
+  },
+  {
+    "revision": "b0d5e98f74820cb22f80",
+    "url": "/static/js/2.e39bd467.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e39bd467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c64d7a89a09431afa9",
+    "url": "/static/js/main.cecf5488.chunk.js"
+  },
+  {
+    "revision": "1543e2cc70a89289eab1",
+    "url": "/static/js/runtime-main.ef44cdbe.js"
+  }
+]);
